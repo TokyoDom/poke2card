5a0f8f36785369d81e5c3557dae5bc787fc577fc
Dom <59603300+TokyoDom@users.noreply.github.com>
2021-11-03 10:24:36 +0900

    cleanup

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -18,47 +18,47 @@ function PokemonCard({ pokemon }) {
   };
 
   return (
-    <section className="container">
-      <div className="header">
-        <h1 className="name">{pokemon.name}</h1>
-        <div className="level">Lv. {pokemon.level ? pokemon.level : 100}</div>
-      </div>
-      <div className="typing">
-        {typing.map((type, i) => (
-          <div className={`type ${type}`} key={i}>
-            {type}
-          </div>
-        ))}
-      </div>
-      <div className="nature">
-        <h2>Nature: </h2>
-        <span>{pokemon.nature}</span>
-      </div>
-      <div className="ability">
-        <h2>Ability: </h2>
-        <span>{pokemon.ability}</span>
-      </div>
-      <div className="item">
-        <h2>Item: </h2>
-        <span>{pokemon.item}</span>
-      </div>
-      <div className="moves-evs">
-        <div className="moves">
-          <h2>Moves:</h2>
-          {pokemon.moves.map((move, i) => (
-            <div key={i}>- {move}</div>
-          ))}
+      <section className="container">
+        <div className="header">
+          <h1 className="name">{pokemon.name}</h1>
+          <div className="level">Lv. {pokemon.level ? pokemon.level : 100}</div>
         </div>
-        <div className="evs">
-          <h2>EVs:</h2>
-          {Object.keys(pokemon.evs).map((stat, i) => (
-            <div key={i}>
-              +{pokemon.evs[stat]} {stat.toUpperCase()}
+        <div className="typing">
+          {typing.map((type, i) => (
+            <div className={`type ${type}`} key={i}>
+              {type}
             </div>
           ))}
         </div>
-      </div>
-    </section>
+        <div className="nature">
+          <h2>Nature: </h2>
+          <span>{pokemon.nature}</span>
+        </div>
+        <div className="ability">
+          <h2>Ability: </h2>
+          <span>{pokemon.ability}</span>
+        </div>
+        <div className="item">
+          <h2>Item: </h2>
+          <span>{pokemon.item}</span>
+        </div>
+        <div className="moves-evs">
+          <div className="moves">
+            <h2>Moves:</h2>
+            {pokemon.moves.map((move, i) => (
+              <div key={i}>- {move}</div>
+            ))}
+          </div>
+          <div className="evs">
+            <h2>EVs:</h2>
+            {Object.keys(pokemon.evs).map((stat, i) => (
+              <div key={i}>
+                +{pokemon.evs[stat]} {stat.toUpperCase()}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
   );
 }
 
